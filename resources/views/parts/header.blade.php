2c966984b2689ad7a7ceaf3c8f196248a754d1c8
aleksejem <alexeyhong10@gmail.com>
2023-11-11 21:26:09 +0500

    Fix words spacing.

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -43,7 +43,7 @@
                     </div>
                 </div>
                 <a href="#footer" class="header-top__btn btn">
-                    <span>{{ trans('landings.call') }}<span>{{ trans('landings.with_us') }}</span></span>
+                    <span>{{ trans('landings.call') }} {{ trans('landings.with_us') }}</span>
                     <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1 17L17 1M17 1H4.2M17 1V13.8" stroke="currentColor" stroke-width="2"
                               stroke-linecap="round" stroke-linejoin="round"/>
