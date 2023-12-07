eb8ca2ff50850db9c8b96a1401abb5f3f558a8bb
aleksejem <alexeyhong10@gmail.com>
2023-12-07 18:55:21 +0500

    Updated amo crm config file.

--- config/amoCRM.php
+++ config/amoCRM.php
@@ -1,9 +1,7 @@
 <?php
 
 return [
-    'amoCRM' => [
-        'clientId' => env('AMOCRM_CLIENT_ID'),
-        'clientSecret' => env('AMOCRM_CLIENT_SECRET'),
-        'redirectUri' => env('AMOCRM_REDIRECT_URI'),
-    ]
+    'clientId' => env('AMOCRM_CLIENT_ID'),
+    'clientSecret' => env('AMOCRM_CLIENT_SECRET'),
+    'redirectUri' => env('AMOCRM_REDIRECT_URI'),
 ];
