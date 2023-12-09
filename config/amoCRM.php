93a2945b5625bacf9c427a17d9338a7c021cc2a0
aleksejem <alexeyhong10@gmail.com>
2023-12-09 19:26:30 +0500

    Added auth code to config.

--- config/amoCRM.php
+++ config/amoCRM.php
@@ -5,4 +5,5 @@ return [
     'clientSecret' => env('AMOCRM_CLIENT_SECRET'),
     'redirectUri' => env('AMOCRM_REDIRECT_URI'),
     'baseDomain' => env('AMOCRM_BASE_DOMAIN'),
+    'authCode' => env('AMOCRM_AUTH_CODE'),
 ];
