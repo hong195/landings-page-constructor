d3e894637b2ba586498517019b7e252aae8b4ee7
aleksejem <alexeyhong10@gmail.com>
2023-12-11 11:23:16 +0500

    Added metrics script.

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -38,10 +38,39 @@
     <meta name="twitter:title" content="">
     <meta name="twitter:description" content="">
     <meta name="twitter:image" content="img/meta.png">
+
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PGBRFL4D');</script>
+    <!-- End Google Tag Manager -->
+
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(95835897, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true,
+            ecommerce:"dataLayer"
+        });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/95835897" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 </head>
 <body>
 
 
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PGBRFL4D"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
 <!-- PRELOADER -->
 
 <div class="preloader">
